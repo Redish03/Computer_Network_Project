74167f292af4c0b303b7d351b4c4eed7ee9131ec
SeongWooHong <pluto0303@naver.com>
2023-12-12 00:51:46 +0900

    feat: add Main UI

--- ComputerNetwork/app/build.gradle.kts
+++ ComputerNetwork/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.computernetwork"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.computernetwork"
