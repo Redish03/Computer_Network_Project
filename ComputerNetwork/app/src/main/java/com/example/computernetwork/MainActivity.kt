4714486ad14059fdc30830f84860010c4b7a1040
SeongWooHong <pluto0303@naver.com>
2023-12-12 01:03:38 +0900

    feat: add set background color change

--- ComputerNetwork/app/src/main/java/com/example/computernetwork/MainActivity.kt
+++ ComputerNetwork/app/src/main/java/com/example/computernetwork/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.computernetwork
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
+import com.example.computernetwork.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
+    }
+
+    private fun changeToRedColor() {
+        binding.stateTextView.text = "경고"
+    }
+
+    private fun changeToYellowColor() {
+        binding.stateTextView.text = "관심"
     }
 }
