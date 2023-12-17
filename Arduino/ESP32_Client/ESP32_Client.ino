2edcbd36627eee4dec1051bea726ec5de36dc45a
SeongWooHong <pluto0303@naver.com>
2023-12-17 16:04:15 +0900

    refactor: add comment and function

--- Arduino/ESP32_Client/ESP32_Client.ino
+++ Arduino/ESP32_Client/ESP32_Client.ino
@@ -3,20 +3,27 @@
 #define WIFI_SSID "AndroidHotspot1440"
 #define WIFI_PASSWORD "84571440"
 
+// 클라이언트의 웹 서버 소켓을 생성한다.
 WebSocketsClient webSocket;
 
+// 건네 받은 데이터그램의 type에 따라서 할 행동들을 정의한다.
 void webSocketEvent(WStype_t type, uint8_t *payload, size_t length)
 {
   switch(type)
   {
+    // 만약 DISCONNECTED가 도착했다면
     case WStype_DISCONNECTED:
+      // Disconnected라는 메세지를 출력하고
       Serial.printf("[WSc] Disconnected\n");
+      // switch 문 탈출
       break;
     case WStype_CONNECTED:
+      // 만약 연결에 성공했다면 아래와 같은 메세지를 출력한다.
       Serial.printf("[WSc] Connected to url: %s\n", payload);
       webSocket.sendTXT("Connected");
       break;
     case WStype_TEXT:
+      // 연결에 성공한 상태에서, payload의 값을 출력한다. 즉, 서버로부터 받은 사용자가 활동하지 않은 시간을 보여준다.
       Serial.println((char*)payload);
       break;
     case WStype_BIN:
@@ -29,6 +36,7 @@ void webSocketEvent(WStype_t type, uint8_t *payload, size_t length)
     }
 }
 
+// 와이파이에 연결한다. 자세한 코드 내용은 Server코드 참고
 void connectWifi(){
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   while(WiFi.status() != WL_CONNECTED) {
@@ -38,12 +46,14 @@ void connectWifi(){
   Serial.println("");
 }
 
+// 웹소켓을 설정한다. begin 함수의 IP에는 서버의 IP와 포트 넘버를 제시한다.
 void webSocketSetting() {
   webSocket.begin("192.168.18.99", 81, "/");
   webSocket.onEvent(webSocketEvent);
   webSocket.setReconnectInterval(5000);
 }
 
+// setup 함수는 아두이노 내장 함수로, loop 함수 실행 전에 한번만 실행된다.
 void setup() {
   // put your setup code here, to run once:
 //  Connect WiFi that same WiFi with connect server
@@ -53,7 +63,9 @@ void setup() {
   delay(1000);
 }
 
+// loop 함수또한 아두이노 내장 함수로, 보드의 전원이 꺼질때까지 계속해서 반복하는 코드이다.
 void loop() {
   // put your main code here, to run repeatedly:
+  // webSocketEvent 함수를 반복 실행한다.
   webSocket.loop();
 }
--- Arduino/ESP32_Server/ESP32_Server.ino
+++ Arduino/ESP32_Server/ESP32_Server.ino
@@ -1,12 +1,16 @@
 #include <WiFi.h>
 #include <WebSocketsServer.h>
 #include <WebServer.h>
+// 와이파이 SSID와 비밀번호, 개발자의 모바일 핫스팟을 사용했다.
 #define WIFI_SSID "AndroidHotspot1440"
 #define WIFI_PASSWORD "84571440"
+// LED와 버튼 제어를 위한 PIN 지정 
 #define BUTTON_PIN 18
 #define LED_PIN 21
 
+// 웹 서버 객체 생성
 WebServer server(80);
+// 소켓 생성
 WebSocketsServer webSocket = WebSocketsServer(81);
 
 int button_state = 0;
@@ -15,7 +19,7 @@ unsigned long unused_time = 0;
 unsigned long prevMillis = 0;
 unsigned long curMillis = 0;
 
-// 전송 HTML
+// 전송할 HTML 파일 
 const char index_html[] PROGMEM = R"rawliteral(
 <!DOCTYPE html>
 <html>
@@ -30,50 +34,76 @@ websock.onmessage=function(evt)
 </html>
 )rawliteral";
 
+// 서버에 index_html 파일 전송
 void handle_root(){
   server.send(200, "text/html", index_html);
 }
 
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(115200);
-  unsigned long ms = millis();
-
-
+// ESP32 모듈에 연결할 LED와 Switch 핀을 지정을 해준다.
+void pin_setting(){
   pinMode(LED_PIN, OUTPUT);
   pinMode(BUTTON_PIN, INPUT_PULLUP);
+}
 
+// 와이파이에 연결한다. 
+void connectWifi(){
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  // 와이파이가 연결될때까지 0.3초마다 .을 시리얼모니터에 찍는다.
   while(WiFi.status()!= WL_CONNECTED) {
     delay(300);
     Serial.print(".");
   }
   Serial.println("");
+  // IP 주소를 시리얼 모니터에 보여준다.
   Serial.print("IP Address: ");
   Serial.println(WiFi.localIP());
+}
 
+// 서버를 열고, 웹소켓도 연다.
+void webServerSocketSetting(){
   server.on("/", handle_root);
 
   server.begin();
   webSocket.begin();
-  
 }
 
+// setup 함수는 아두이노 내장 함수로, loop 함수 실행 전에 한번만 실행된다.
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  pin_setting();
+  connectWifi();
+  webServerSocketSetting();
+}
+// 전송하기 전 버퍼 생성
 char buffer[100];
+
+// loop 함수또한 아두이노 내장 함수로, 보드의 전원이 꺼질때까지 계속해서 반복하는 코드이다.
 void loop() {
   // put your main code here, to run repeatedly:
+  // 버튼의 상태를 읽어온다. 즉, 누른 상태인지 아닌지 확인한다.
   button_state = digitalRead(BUTTON_PIN);
+
+  // 버튼이 눌린 상태라면
   if(button_state == LOW) {
+    // LED를 켠다.
     digitalWrite(LED_PIN, HIGH);
+    // prevMillis는 버튼이 마지막으로 눌린 시간을 기록한다.
     prevMillis = millis();
   }
+  // 버튼이 눌리지 않은 상태라면
   else {
+    // LED를 끈다.
     digitalWrite(LED_PIN, LOW);
   }
+  // 현재 초를 저장한다.
   curMillis = millis();
+  // 현재 초와 마지막으로 버튼을 누른 시간을 빼서 사용자가 사용하지 않은 시간을 구한다. 
   unused_time = curMillis - prevMillis;
-  
+
+  // 서버가 Client와 연결을 활성화 한다.
   server.handleClient();  
+  // 서버 웹소켓이 작동한다.
   webSocket.loop();
 
   // 전송 값 설정
